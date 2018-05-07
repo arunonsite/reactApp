1650cbd28cd8029bb1f2ae74a829cc8f32a9c56d
arunonsite <arunonsite@gmail.com>
2018-05-07 18:57:38 +0530

    search bar api added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-// create components
-const App =  () => {
-    return (
-    <div>
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search';
+const API_KEY ='AIzaSyBRhtCxGt3IxdlEX0KHT0KM5P5Abevys9g';
+
+
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {videos: []};
+        YTSearch({Key: API_KEY,term: 'vijay'},(videos) =>{
+            this.setState({ videos });
+            //this.setState({ videos : videos});
+            });
+    }
+
+    render(){
+        return (
+            <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
     </div>
-    ); 
+        );
+    }
+    
 }
+
+
 
 // Components generate to HTML to the page (DOM)
 
